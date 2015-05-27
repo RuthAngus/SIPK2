b27893142c64b3a4342b0318faafb795e1774ea4
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-05-26 20:56:58 -0400

    renaming and adding eval_1_freq

--- SIP.py
+++ SIP.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # loop over eval_freq to compute a periodogram
-def K2pgram(x, y, basis, fs):
+def SIP(x, y, basis, fs):
     """
     # `K2pgram`
 
@@ -17,7 +17,7 @@ def K2pgram(x, y, basis, fs):
     s2n = np.zeros_like(fs)
     for i, f in enumerate(fs):
         amps2[i], s2n[i], w = eval_freq(x, y, f, AT, ATA)
-    return s2n
+    return s2n, amps2, w
 
 # calculate periodogram by just updating sin and cos parts of the  matrices
 def eval_freq(x, y, f, AT, ATA, compute_trends=False):
@@ -49,4 +49,44 @@ def eval_freq(x, y, f, AT, ATA, compute_trends=False):
     if compute_trends:
         trends = np.dot(w[:-2], AT[:-2])
         return np.sum(w[-2:]**2), s2n, trends
-    return np.sum(w[-2:]**2), s2n, w
+    return s2n, np.sum(w[-2:]**2), w
+
+# compute trends at a certain frequency.
+def eval_1_freq(x, y, f, compute_trends=True):
+    """
+    # `eval_1_freq`
+
+    Computes the sum of squared amplitudes for a sine and cosine function
+    with frequency, f.
+    Doesn't use precomputed matrices so is slow compared to eval_freq.
+    This version is just for computing conditional light curves.
+    It will provide the same output as eval_freq if you give it the
+    pre-constructed matrices.
+    """
+
+    # construct arrays
+    AT = np.concatenate((basis, np.ones((3, len(y)))), axis=0)
+    ATA = np.dot(AT, AT.T)
+
+    arg = 2*np.pi*f*x
+    AT[-2, :] = np.sin(arg)
+    AT[-1, :] = np.cos(arg)
+
+    # AT.shape = (153, nt)
+    # shape: (151, nt) * (nt, 2) -> (151, 2)
+    v = np.dot(AT[:-2, :], AT[-2:, :].T)
+    ATA[:-2, -2:] = v
+    ATA[-2:, :-2] = v.T
+
+    # AT[-2:, :].shape = (2, nt)
+    # (2, nt), (nt, 2)
+    ATA[-2:, -2:] = np.dot(AT[-2:, :], AT[-2:, :].T)
+    w = np.linalg.solve(ATA, np.dot(AT, y))
+
+    S = np.linalg.inv(ATA)[-2:, -2:]
+    s2n = np.dot(w[-2:], np.linalg.solve(S, w[-2:]))
+
+    if compute_trends:
+        trends = np.dot(w[:-2], AT[:-2])
+        return np.sum(w[-2:]**2), s2n, trends
+    return s2n, np.sum(w[-2:]**2), w
