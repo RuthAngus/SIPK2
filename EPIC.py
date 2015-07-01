c7d530adf42fd880235cd15fd7ee9f7c6e847462
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-07-01 17:28:01 -0400

    adding EPIC.py

---
+++ EPIC.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import fitsio
+from SIP import SIP, eval_freq
+
+def EPICSIP(EPIC, C, pmin=.01, pmax=100, nELC=150, plot=False):
+    '''
+    Given an EPIC ID, campaign number and minimum and maximum period,
+    produce a sip.
+    '''
+    fname = "ktwo%s-c%s_lpd-lc.fits" % (str(int(EPIC)), str(int(C)).zfill(2))
+    print fname, "found"
+
+    # load the data
+    data = fitsio.read(fname)
+    aps = fitsio.read(fname, 2)
+    y = data["flux"][:, np.argmin(aps["cdpp6"])]
+    x = data["time"]
+    q = data["quality"]
+    m = np.isfinite(y) * np.isfinite(x) * (q==0)
+    y, x = y[m], x[m]
+    y = y / np.median(y) - 1
+
+    nELC = 150
+    with h5py.File("c%s.h5" % C, "r") as f:
+        basis = f["basis"][:nELC, m]
+
+    if plot:
+        plt.clf()
+        plt.plot(x, y, "k")
+        plt.xlabel("BJD - 2454833 (days)")
+        plt.ylabel("Normalised Flux")
+        plt.savefig("%s_lc" % EPIC)
+
+    periods = np.arange(pmin, pmax, .1*pmin)
+    freqs = 1./periods
+    s2n, amp2s, w = SIP(x, y, basis, freqs)
+
+    if plot:
+        plt.clf()
+        plt.plot(periods, amp2s/max(amp2s), "r", label="amp2s")
+        plt.plot(periods, s2n/max(s2n), "k", label="s2n")
+        plt.legend()
+        plt.xlabel("Period (days)")
+        plt.ylabel("Relative (S/N)^2")
+        plt.savefig("%s_sip" % EPIC)
+
+    return x, y, periods, s2n, amp2s, w
